e68e8fed865ad2388acf2b485b418ec8588e1d67
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-29 00:42:18 +0530

    Updated provider props and tested themes.

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,10 +1,12 @@
+import ThemeTogglerButton from "@/components/custom/theme-toggler";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-white dark:bg-gray-800 w-full h-full">
       <Link href={"/login"}>Login</Link>
       <Link href={"/signup"}>Signup</Link>
+      <ThemeTogglerButton />
     </main>
   );
 }
