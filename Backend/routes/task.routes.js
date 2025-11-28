9901278c95290e47c7d7e5f2e2a60419bbd3d056
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-28 17:38:49 +0530

    updated route.

--- Backend/routes/task.routes.js
+++ Backend/routes/task.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { createTask, fetchAllTasks } from "../controllers/task.controller.js";
+import { createTask, fetchAllTasks, updateTask } from "../controllers/task.controller.js";
 
 const Router = express.Router();
 
-Router.get('/all', fetchAllTasks).post('/create', createTask);
+Router.get('/all', fetchAllTasks).post('/create', createTask).put('/:id', updateTask);
 
 export default Router;
