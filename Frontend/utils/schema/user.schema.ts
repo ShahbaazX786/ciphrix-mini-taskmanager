082144a9cae5bb1607042215b426fac09284ab47
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-29 00:27:42 +0530

    Added form schema

---
+++ Frontend/utils/schema/user.schema.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+const loginFormSchema = z.object({
+  email: z.email({ error: "Please enter a valid email address" }),
+  password: z
+    .string()
+    .min(8, { error: "Password must be atleast 8 characters long" })
+    .max(50, { error: "Password must not exceed 50 characters" }),
+});
+
+const signupFormSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, { message: "First name must be greater than 2 letters" })
+      .max(50),
+
+    lastName: z
+      .string()
+      .min(2, { message: "Last name must be greater than 2 letters" })
+      .max(50),
+
+    email: z.string().email({ message: "Please enter a valid email address" }),
+
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .max(50),
+
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
+
+export { loginFormSchema, signupFormSchema };
