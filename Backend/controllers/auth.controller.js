9fe2019d62ebb88936d6873d39a426f417d2e3bd
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-28 15:51:21 +0530

    Added signin controller method.

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
+import User from "../models/user.model.js";
 
-export const SignUp = async (req, res) => {
+const SignUp = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         if (!email || !password || !name) {
@@ -25,3 +25,35 @@ export const SignUp = async (req, res) => {
         res.status(400).json({ success: false, message: error.message });
     }
 }
+
+
+const SignIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            throw new Error('Email and Password are required');
+        }
+        const user = await User.findOne({ email });
+
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: "Invalid Email or User not Found" });
+        }
+
+        const isPasswordMatching = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatching) {
+            return res.status(404).json({ success: false, message: "Invalid Password" });
+        }
+
+        return res.status(200).json({
+            success: true, message: "User LoggedIn Sucessfully"
+        })
+
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
+
+export { SignUp, SignIn };
