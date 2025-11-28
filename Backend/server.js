b64767cf715ac38cfbc506133c2edfff119c575f
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-28 12:54:55 +0530

    initial server setup.

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 7200;
+app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
+
+// Initial Route
+app.get('/', (_req, res) => {
+    res.send('Server is up and running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
