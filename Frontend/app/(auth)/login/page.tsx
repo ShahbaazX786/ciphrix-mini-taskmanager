abb647d277b664caa6ba2c970c0c72212e4e786f
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-28 19:33:17 +0530

    Updated authform with dynamic props.

--- Frontend/app/(auth)/login/page.tsx
+++ Frontend/app/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
+import AuthForm from "../components/AuthForm";
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <AuthForm mode={"login"} />
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
