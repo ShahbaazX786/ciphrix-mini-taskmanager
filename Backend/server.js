7ade358bdff52616181efeb1cd9fecded738f1c9
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-28 15:47:07 +0530

    added express json middleware, and defined route in server.

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,20 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { connectDB } from './utils/ConnectDB.js';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 7200;
 app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
+app.use(express.json());
 
 // Initial Route
 app.get('/', (_req, res) => {
     res.send('Server is up and running');
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, async () => {
     await connectDB();
