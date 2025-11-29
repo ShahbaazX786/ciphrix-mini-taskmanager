d8e4efcd46aa021b42440cef899f5d8ca9d7a12d
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-29 11:20:53 +0530

    Added datatable in dashboard page.

---
+++ Frontend/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import { columns } from "../components/columns";
+import { DataTable } from "../components/data-table";
+
+const dummyTasks = [
+  {
+    _id: "69298be0b54d4d4fd3be3b91",
+    title: "Do a Prank",
+    description: "Run!......",
+    status: "Completed",
+    createdAt: "2025-11-28T11:47:44.886Z",
+    updatedAt: "2025-11-28T12:08:11.644Z",
+    __v: 0,
+  },
+  {
+    _id: "69298be0b54d4d4fd3be3b92",
+    title: "Unsubscribe All stupid channels",
+    description: "yoi yoii yoiii",
+    status: "Pending",
+    createdAt: "2025-11-28T11:47:44.886Z",
+    updatedAt: "2025-11-28T12:08:11.644Z",
+    __v: 0,
+  },
+];
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <DataTable columns={columns} data={dummyTasks} />
+    </div>
+  );
+};
+
+export default DashboardPage;
