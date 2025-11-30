efce53c181669a7243927cfdd304a50a5859f963
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-30 23:20:37 +0530

    updated user model schema.

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -14,6 +14,19 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    otp: {
+        type: String,
+    },
+    otpExpiryTime: Date,
 }, { timestamps: true })
 
 const User = mongoose.models.user || mongoose.model('User', userSchema);
