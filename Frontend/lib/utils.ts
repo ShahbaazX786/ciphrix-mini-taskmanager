b0130fbdbe56b94bc3eb4586129ac818aa3747f3
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-30 12:16:32 +0530

    merged helpers in utils.

--- Frontend/lib/utils.ts
+++ Frontend/lib/utils.ts
@@ -1,6 +1,42 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { projectData } from "./constants";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getAppName = () => {
+  return projectData["appName"];
+};
+
+export const getFormattedDate = (timestamp: string) => {
+  const date = new Date(timestamp);
+
+  const day = String(date.getDate()).padStart(2, "0");
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const monthName = months[date.getMonth()];
+
+  const year = date.getFullYear();
+
+  let hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+
+  return `${day} ${monthName} ${year}, ${hours}:${minutes} ${ampm}`;
+};
