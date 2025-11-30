b18afe528137fd53f99279091d71768a4fc71952
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-30 23:19:01 +0530

    Utilizing cookie parser middleware.

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,27 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import { connectDB } from './utils/ConnectDB.js';
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/task.routes.js';
+import { connectDB } from './utils/ConnectDB.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 7200;
+
+// middlewares
 app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
 app.use(express.json());
+app.use(cookieParser())
+app.use('/api/auth', authRoutes);
+app.use('/api/task', taskRoutes);
 
 // Initial Route
 app.get('/', (_req, res) => {
     res.send('Server is up and running');
 });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/task', taskRoutes);
 
 app.listen(PORT, async () => {
     await connectDB();
