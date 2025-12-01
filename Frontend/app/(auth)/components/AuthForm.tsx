04b0f8eef427b214dda8877b37c6bf332ba42554
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-01 11:21:19 +0530

    Added countdown timer component.

--- Frontend/app/(auth)/components/AuthForm.tsx
+++ Frontend/app/(auth)/components/AuthForm.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { FieldDescription } from "@/components/ui/field";
-import { getAppName } from "@/lib/helpers";
+import { getAppName } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import LoginForm from "./LoginForm";
---
+++ Frontend/app/(auth)/components/CountdownTimer.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const CountdownTimer = () => {
+  const [timeLeft, setTimeLeft] = useState(15);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const startCountdown = useCallback(() => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((prev) => {
+        if (prev <= 1) {
+          clearInterval(intervalRef.current!);
+          intervalRef.current = null;
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    startCountdown();
+
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+  }, [startCountdown]);
+
+  const formatTime = useCallback((seconds: number) => {
+    const m = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const s = Math.floor(seconds % 60)
+      .toString()
+      .padStart(2, "0");
+    return `${m}:${s}`;
+  }, []);
+
+  return (
+    <p className="text-black dark:text-white">
+      {timeLeft ? (
+        <span>
+          OTP expires in{" "}
+          <span
+            className={cn({
+              "text-red-500": timeLeft < 60,
+              "text-yellow-500": timeLeft >= 60 && timeLeft < 180,
+              "text-black dark:text-white": timeLeft >= 180,
+            })}
+          >
+            {formatTime(timeLeft)}
+          </span>
+        </span>
+      ) : (
+        <span>OTP Expired....</span>
+      )}
+    </p>
+  );
+};
+
+export default CountdownTimer;
