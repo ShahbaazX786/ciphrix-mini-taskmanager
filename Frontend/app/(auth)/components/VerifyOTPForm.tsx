62b887414c4439d5f6ad219f0c33ba74b4050e0a
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-01 11:20:12 +0530

    Added verifyOTP form.

---
+++ Frontend/app/(auth)/components/VerifyOTPForm.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { verifyOTP } from "@/lib/api/api.auth";
+import { verifyOTPFormSchema } from "@/lib/schema/user.schema";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import CountdownTimer from "./CountdownTimer";
+
+const VerifyOTPForm = () => {
+  const router = useRouter();
+
+  const otpForm = useForm<z.infer<typeof verifyOTPFormSchema>>({
+    resolver: zodResolver(verifyOTPFormSchema),
+    defaultValues: {
+      otp: "",
+    },
+  });
+
+  const verifyOTPMutation = useMutation({
+    mutationFn: (data: z.infer<typeof verifyOTPFormSchema>) => verifyOTP(data),
+    onSuccess: (res) => {
+      if (res?.success) {
+        console.warn("User Verified Sucessfully");
+        router.push("/dashboard");
+      } else {
+        console.warn("Something went wrong i guess", res?.message);
+      }
+    },
+    onError: (err: any) => {
+      console.warn("Error", err?.response?.data?.message);
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof verifyOTPFormSchema>) => {
+    verifyOTPMutation.mutate(data);
+  };
+
+  return (
+    <form onSubmit={otpForm.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+      <FieldGroup>
+        <Field>
+          <FieldLabel htmlFor="inputOTP" className="text-lg">
+            <CountdownTimer />
+          </FieldLabel>
+
+          <Controller
+            name="otp"
+            control={otpForm.control}
+            render={({ field }) => (
+              <InputOTP
+                maxLength={6}
+                value={field.value}
+                onChange={field.onChange}
+              >
+                <InputOTPGroup
+                  className={`${
+                    otpForm.formState.errors.otp ? "text-red-500" : ""
+                  }`}
+                >
+                  <InputOTPSlot index={0} />
+                  <InputOTPSlot index={1} />
+                  <InputOTPSlot index={2} />
+                  <InputOTPSlot index={3} />
+                  <InputOTPSlot index={4} />
+                  <InputOTPSlot index={5} />
+                </InputOTPGroup>
+              </InputOTP>
+            )}
+          />
+
+          {otpForm.formState.errors.otp && (
+            <p className="text-red-500 text-sm mt-1">
+              {otpForm.formState.errors.otp.message}
+            </p>
+          )}
+        </Field>
+      </FieldGroup>
+
+      <Button
+        type="submit"
+        disabled={Boolean(otpForm.formState.errors.otp?.message)}
+      >
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default VerifyOTPForm;
