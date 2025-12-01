0d78c8343338eef99d69b4c18f607c436c2b722b
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-01 08:53:13 +0530

    Updated routes with middleware + new routes.

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { SignUp, SignIn, SignOut } from '../controllers/auth.controller.js';
+import { getAllUsers, getUser, SignIn, SignOut, SignUp, VerifyOTP } from '../controllers/auth.controller.js';
+import { checkToken } from '../middleware/checkToken.js';
 
 const Router = express.Router();
 
-Router.post('/signup', SignUp).post('/signin', SignIn).post('/signout', SignOut);
+Router.post('/signup', SignUp).post('/signin', SignIn).post('/signout', SignOut).post('/verify-otp', checkToken, VerifyOTP);
+Router.get('/', getAllUsers);
+Router.get('/:id', getUser);
 
 export default Router;
