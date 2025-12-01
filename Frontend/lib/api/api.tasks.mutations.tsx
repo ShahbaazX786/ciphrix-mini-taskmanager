ef1c1459b116eb39de27be73d5ed8069adb2f2d6
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-01 11:25:50 +0530

    Updated create and update mutations.

--- Frontend/lib/api/api.tasks.mutations.tsx
+++ Frontend/lib/api/api.tasks.mutations.tsx
@@ -1,6 +1,9 @@
 // Hooks to use zustand mutations to execute api calls.
 
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import z from "zod";
+import { editTaskSchema, newTaskSchema } from "../schema/task.schema";
 import { useTaskStore } from "../store/task.store";
 import { createTask, deleteTask, fetchAllTasks, updateTask } from "./api.task";
 
@@ -12,7 +15,6 @@ const useTasks = () => {
     queryKey: ["tasks", page, limit],
     queryFn: async () => {
       const res = await fetchAllTasks(page, limit);
-      console.warn(res);
       if (res.success) {
         setTotalPages(res.totalPages);
       }
@@ -23,19 +25,39 @@ const useTasks = () => {
   });
 
   const createTaskMutation = useMutation({
-    mutationFn: createTask,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["tasks"] }),
+    mutationFn: async (data: z.infer<typeof newTaskSchema>) => {
+      return toast.promise(createTask(data), {
+        loading: "Creating Task...",
+        success: "Task Created Sucessfully!",
+        error: (err) => err?.response?.data?.message || "Failed to Create Task",
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks", page, limit] });
+    },
   });
 
   const updateTaskMutation = useMutation({
-    mutationFn: ({ id, payload }: { id: string; payload?: any }) =>
-      updateTask(id, payload),
+    mutationFn: async ({
+      id,
+      payload,
+    }: {
+      id: string;
+      payload: z.infer<typeof editTaskSchema>;
+    }) => {
+      return toast.promise(updateTask(id, payload), {
+        loading: "Updating Task...",
+        success: "Task Updated Sucessfully!",
+        error: (err) => err?.response?.data?.message || "Failed to Update Task",
+      });
+    },
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["tasks"] }),
   });
 
   const deleteTaskMutation = useMutation({
     mutationFn: (id: string) => deleteTask(id),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["tasks"] }),
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["tasks"], exact: false }),
   });
 
   return {
