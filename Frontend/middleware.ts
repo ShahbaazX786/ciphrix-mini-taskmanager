1c4d5874b57c1da8fe40ccb96394166a8924a1cd
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-01 17:26:07 +0530

    Added token checking middleware to protect routes in UI.

---
+++ Frontend/middleware.ts
@@ -0,0 +1,31 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const checkValidSession = (req: NextRequest) => {
+  const token = req.cookies.get("tm-token")?.value;
+
+  const isAuth = !!token;
+
+  const AuthPages = ["/login", "/signup"];
+  const isAuthPage = AuthPages.includes(req.nextUrl.pathname);
+
+  if (isAuth && isAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  const protectedRoutes = ["/dashboard"];
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    req.nextUrl.pathname.startsWith(route)
+  );
+
+  if (!isAuth && isProtectedRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/login", "/signup", "/dashboard/:path*"],
+};
+
+export default checkValidSession;
