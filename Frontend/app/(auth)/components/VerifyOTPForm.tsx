bbe2a532c90974d2925a536526d36bab678a54fd
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-02 20:37:20 +0530

    opens dialog when success.

--- Frontend/app/(auth)/components/VerifyOTPForm.tsx
+++ Frontend/app/(auth)/components/VerifyOTPForm.tsx
@@ -13,15 +13,15 @@ import {
 } from "@/components/ui/input-otp";
 import { verifyOTP } from "@/lib/api/api.auth";
 import { verifyOTPFormSchema } from "@/lib/schema/user.schema";
-import { useAuthStore } from "@/lib/store/auth.store";
+import { mutationError, verifyOTPResponse } from "@/lib/types";
 import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { toast } from "sonner";
 import CountdownTimer from "./CountdownTimer";
+import SignupSuccessDialog from "./SignupSuccessDialog";
 
 const VerifyOTPForm = () => {
-  const { setIsAuthenticated, setTokenExpiry } = useAuthStore();
-  const router = useRouter();
+  const [open, setOpen] = useState(false);
 
   const otpForm = useForm<z.infer<typeof verifyOTPFormSchema>>({
     resolver: zodResolver(verifyOTPFormSchema),
@@ -32,27 +32,22 @@ const VerifyOTPForm = () => {
 
   const verifyOTPMutation = useMutation({
     mutationFn: (data: z.infer<typeof verifyOTPFormSchema>) => verifyOTP(data),
-    onSuccess: (res) => {
+    onMutate: () => {
+      toast.loading("Verifying One Time Password...");
+    },
+    onSuccess: (res: verifyOTPResponse) => {
       if (res?.success) {
-        setIsAuthenticated(true);
-        setTokenExpiry(res?.tokenExpiry);
+        toast.dismiss();
         toast.success(res?.message, {
           richColors: true,
         });
-        router.push("/dashboard");
-      } else {
-        toast.error(res?.message, {
-          richColors: true,
-        });
-        console.warn("Something went wrong i guess", res?.message);
       }
     },
-    onError: (err: any) => {
+    onError: (err: mutationError) => {
+      toast.dismiss();
       toast.error(err?.response?.data?.message, {
         richColors: true,
       });
-      setIsAuthenticated(false);
-      setTokenExpiry(0);
       console.warn("Error", err?.response?.data?.message);
     },
   });
@@ -62,53 +57,59 @@ const VerifyOTPForm = () => {
   };
 
   return (
-    <form onSubmit={otpForm.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-      <FieldGroup>
-        <Field>
-          <FieldLabel htmlFor="inputOTP" className="text-lg">
-            <CountdownTimer />
-          </FieldLabel>
-
-          <Controller
-            name="otp"
-            control={otpForm.control}
-            render={({ field }) => (
-              <InputOTP
-                maxLength={6}
-                value={field.value}
-                onChange={field.onChange}
-              >
-                <InputOTPGroup
-                  className={`${
-                    otpForm.formState.errors.otp ? "text-red-500" : ""
-                  }`}
-                >
-                  <InputOTPSlot index={0} />
-                  <InputOTPSlot index={1} />
-                  <InputOTPSlot index={2} />
-                  <InputOTPSlot index={3} />
-                  <InputOTPSlot index={4} />
-                  <InputOTPSlot index={5} />
-                </InputOTPGroup>
-              </InputOTP>
-            )}
-          />
-
-          {otpForm.formState.errors.otp && (
-            <p className="text-red-500 text-sm mt-1">
-              {otpForm.formState.errors.otp.message}
-            </p>
-          )}
-        </Field>
-      </FieldGroup>
-
-      <Button
-        type="submit"
-        disabled={Boolean(otpForm.formState.errors.otp?.message)}
+    <>
+      <form
+        onSubmit={otpForm.handleSubmit(onSubmit)}
+        className="w-2/3 space-y-6"
       >
-        Submit
-      </Button>
-    </form>
+        <FieldGroup>
+          <Field>
+            <FieldLabel htmlFor="inputOTP" className="text-lg">
+              <CountdownTimer />
+            </FieldLabel>
+
+            <Controller
+              name="otp"
+              control={otpForm.control}
+              render={({ field }) => (
+                <InputOTP
+                  maxLength={6}
+                  value={field.value}
+                  onChange={field.onChange}
+                >
+                  <InputOTPGroup
+                    className={`${
+                      otpForm.formState.errors.otp ? "text-red-500" : ""
+                    } bg-white dark:bg-gray-700`}
+                  >
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+              )}
+            />
+
+            {otpForm.formState.errors.otp && (
+              <p className="text-red-500 text-sm mt-1">
+                {otpForm.formState.errors.otp.message}
+              </p>
+            )}
+          </Field>
+        </FieldGroup>
+
+        <Button
+          type="submit"
+          disabled={Boolean(otpForm.formState.errors.otp?.message)}
+        >
+          Submit
+        </Button>
+      </form>
+      <SignupSuccessDialog open={open} onOpenChange={setOpen} />
+    </>
   );
 };
 
