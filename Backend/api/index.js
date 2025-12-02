8d72f32b56cd00a72685f000c3d18938a430cb1e
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-03 00:40:18 +0530

    fixing mongodb vercel deployment 2

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,4 +1,26 @@
-import app from "../server.js";
+import express from "express";
 import serverless from "serverless-http";
+import dotenv from "dotenv";
+import { connectDB } from "../config/db.js";
 
-export default serverless(app);
+import authRoutes from '../routes/auth.routes.js';
+import taskRoutes from '../routes/task.routes.js';
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+await connectDB();
+
+const app = express();
+
+app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.get("/", (_req, res) => {
+    res.send("Backend is up and running");
+});
+
+export const handler = serverless(app);
+export default handler;
--- Backend/utils/ConnectDB.js
+++ Backend/utils/ConnectDB.js
@@ -1,15 +1,31 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//     try {
+//         const connectionStatus = await mongoose.connect(process.env.MONGODB_URI);
+//         if (connectionStatus?.connection?.host) {
+//             console.log(`Sucessfully Connected to DB at :${connectionStatus?.connection?.host}`);
+//         } else {
+//             console.log(`Error While connecting to the DB: ${connectionStatus?.connection}`);
+//         }
+//     } catch (error) {
+//         console.error('Error Connecting To DB:', error.message);
+//         process.exit(1);
+//     }
+// }
+
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+    if (isConnected) return;
+
     try {
-        const connectionStatus = await mongoose.connect(process.env.MONGODB_URI);
-        if (connectionStatus?.connection?.host) {
-            console.log(`Sucessfully Connected to DB at :${connectionStatus?.connection?.host}`);
-        } else {
-            console.log(`Error While connecting to the DB: ${connectionStatus?.connection}`);
-        }
-    } catch (error) {
-        console.error('Error Connecting To DB:', error.message);
-        process.exit(1);
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        isConnected = conn.connection.readyState === 1;
+        console.log("🟢 MongoDB Connected:", conn.connection.host);
+    } catch (err) {
+        console.error("🔴 DB Error:", err.message);
     }
-}
+};
