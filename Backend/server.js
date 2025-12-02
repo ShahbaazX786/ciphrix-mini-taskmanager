22c0547aa931b3a57c7f38d12f278688cfff141a
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-02 22:31:03 +0530

    Testing vercel build - 5

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,11 @@
-import './utils/dotenvConfig.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import http from 'http';
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/task.routes.js';
+import { connectDB } from './utils/ConnectDB.js';
+import './utils/dotenvConfig.js';
 
 const app = express();
 const server = http.createServer(app);
@@ -12,20 +14,18 @@ const server = http.createServer(app);
 app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
 app.use(express.json());
 app.use(cookieParser())
+
+// Routes
+app.use('/api/status', (_req, res) => res.send('Server is up and Running'));
 app.use('/api/auth', authRoutes);
 app.use('/api/task', taskRoutes);
-
-// Initial Route
-app.get('/', (_req, res) => {
-    res.send('Server is up and running');
-});
 
 // DB Connection
 await connectDB();
 
 // Env Checking and local app Startup.
 if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 7200;
+    const PORT = process.env.PORT || 3000;
     server.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
 }
 
