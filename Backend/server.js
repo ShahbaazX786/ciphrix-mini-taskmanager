7b78e4612d12fb6e611361491cb07dd0431105f9
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-02 22:43:08 +0530

    Testing vercel build - 6

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,36 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import http from 'http';
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/task.routes.js';
 import { connectDB } from './utils/ConnectDB.js';
 import './utils/dotenvConfig.js';
 
 const app = express();
-const server = http.createServer(app);
 
 // middlewares
-app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
+app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Routes
-app.use('/api/status', (_req, res) => res.send('Server is up and Running'));
+app.get('/api/status', (_req, res) => res.send('Server is up and Running'));
 app.use('/api/auth', authRoutes);
 app.use('/api/task', taskRoutes);
 
-// DB Connection
-await connectDB();
+// Lazy DB connect
+let isConnected = false;
 
-// Env Checking and local app Startup.
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 3000;
-    server.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
+async function ensureDB() {
+    if (!isConnected) {
+        await connectDB();
+        isConnected = true;
+    }
 }
 
-// Otherwise vercel will reuse the server and listen to it itself.
-export default server;
+app.use(async (_req, _res, next) => {
+    await ensureDB();
+    next();
+});
+
+export default app;
