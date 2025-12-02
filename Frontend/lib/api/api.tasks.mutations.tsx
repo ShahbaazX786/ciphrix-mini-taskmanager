cc6a810979716c199eca26e3b1fa96d7ed5cec18
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-02 10:37:00 +0530

    updated task mutations.

--- Frontend/lib/api/api.tasks.mutations.tsx
+++ Frontend/lib/api/api.tasks.mutations.tsx
@@ -5,35 +5,42 @@ import { toast } from "sonner";
 import z from "zod";
 import { editTaskSchema, newTaskSchema } from "../schema/task.schema";
 import { useTaskStore } from "../store/task.store";
+import { mutationError, taskQueryResponse } from "../types";
 import { createTask, deleteTask, fetchAllTasks, updateTask } from "./api.task";
 
-const useTasks = () => {
+const useTaskQuery = () => {
   const queryClient = useQueryClient();
-  const { page, limit, setTotalPages } = useTaskStore();
+  const { page, limit, setPage, setTotalPages, setTasks } = useTaskStore();
 
-  const tasksQuery = useQuery({
-    queryKey: ["tasks", page, limit],
-    queryFn: async () => {
-      const res = await fetchAllTasks(page, limit);
-      if (res.success) {
-        setTotalPages(res.totalPages);
+  const taskQuery = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async (): Promise<taskQueryResponse> => {
+      const data: taskQueryResponse = await fetchAllTasks(page, limit);
+      if (data?.success) {
+        setPage(data?.page);
+        setTotalPages(data?.totalPages);
+        setTasks(data?.taskList);
       }
-      return res;
+      return data;
     },
-    retry: 3,
-    retryDelay: 3000,
   });
 
   const createTaskMutation = useMutation({
     mutationFn: async (data: z.infer<typeof newTaskSchema>) => {
-      return toast.promise(createTask(data), {
-        loading: "Creating Task...",
-        success: "Task Created Sucessfully!",
-        error: (err) => err?.response?.data?.message || "Failed to Create Task",
-      });
+      const result = await createTask(data);
+      return result;
+    },
+    onMutate: () => {
+      toast.loading("Creating Task...");
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["tasks", page, limit] });
+      toast.dismiss();
+      toast.success("Task Created Successfully!");
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: (err: mutationError) => {
+      toast.dismiss();
+      toast.error(err?.response?.data?.message || "Failed to create task");
     },
   });
 
@@ -45,27 +52,42 @@ const useTasks = () => {
       id: string;
       payload: z.infer<typeof editTaskSchema>;
     }) => {
-      return toast.promise(updateTask(id, payload), {
-        loading: "Updating Task...",
-        success: "Task Updated Sucessfully!",
-        error: (err) => err?.response?.data?.message || "Failed to Update Task",
-      });
+      const result = await updateTask(id, payload);
+      return result;
     },
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["tasks"] }),
+    onMutate: () => {
+      toast.loading("Updating task...");
+    },
+    onSuccess: () => {
+      toast.dismiss();
+      toast.success("Task updated successfully!");
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: (err: mutationError) => {
+      toast.dismiss();
+      toast.error(err?.response?.data?.message || "Failed to update task");
+    },
   });
 
   const deleteTaskMutation = useMutation({
-    mutationFn: (id: string) => deleteTask(id),
-    onSuccess: () =>
-      queryClient.invalidateQueries({ queryKey: ["tasks"], exact: false }),
+    mutationFn: async (id: string) => {
+      return await deleteTask(id);
+    },
+    onSuccess: () => {
+      toast.success("Task deleted successfully!");
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: (err: mutationError) => {
+      toast.error(err?.response?.data?.message || "Failed to delete task");
+    },
   });
 
   return {
-    tasksQuery,
+    taskQuery,
     createTaskMutation,
     updateTaskMutation,
     deleteTaskMutation,
   };
 };
 
-export default useTasks;
+export default useTaskQuery;
