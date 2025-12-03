61b0c57d64644a18f5ba4454b3eec7c892f6d50c
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-03 08:54:04 +0530

    bug fix: testing vercel build failure.

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,27 +1,50 @@
 import express from "express";
 import cors from 'cors';
-import serverless from "serverless-http";
 import dotenv from "dotenv";
-import { connectDB } from '../utils/ConnectDB.js';
+import cookieParser from "cookie-parser";
 
 import authRoutes from '../routes/auth.routes.js';
 import taskRoutes from '../routes/task.routes.js';
-import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 
 dotenv.config();
-await connectDB();
+
 
 const app = express();
 
 app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
+// await connectDB();
+
+let isConnected = false;
+async function connectToDB() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        isConnected = true;
+        console.log('Database connected');
+    } catch (error) {
+        console.error('Error connecting to DB', error);
+    }
+
+}
+
+app.use((_req, _res, next) => {
+    if (isConnected) {
+        connectToDB();
+    }
+    next();
+})
 
 app.get("/", (_req, res) => {
     res.send("Backend is up and running");
 });
 
-export const handler = serverless(app);
-export default handler;
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,28 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express from 'express';
-import authRoutes from './routes/auth.routes.js';
-import taskRoutes from './routes/task.routes.js';
-import { connectDB } from './utils/ConnectDB.js';
-import './utils/dotenvConfig.js';
+// import cookieParser from 'cookie-parser';
+// import cors from 'cors';
+// import express from 'express';
+// import authRoutes from './routes/auth.routes.js';
+// import taskRoutes from './routes/task.routes.js';
+// import { connectDB } from './utils/ConnectDB.js';
+// import './utils/dotenvConfig.js';
 
-const app = express();
-const PORT = process.env.PORT || 7200;
+// const app = express();
+// const PORT = process.env.PORT || 7200;
 
-// middlewares
-await connectDB();
-app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
-app.use(express.json());
-app.use(cookieParser())
-app.use('/api/auth', authRoutes);
-app.use('/api/task', taskRoutes);
+// // middlewares
+// await connectDB();
+// app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
+// app.use(express.json());
+// app.use(cookieParser())
+// app.use('/api/auth', authRoutes);
+// app.use('/api/task', taskRoutes);
 
-// Initial Route
-app.get('/', (_req, res) => {
-    res.send('Server is up and running');
-});
+// // Initial Route
+// app.get('/', (_req, res) => {
+//     res.send('Server is up and running');
+// });
 
 
-app.listen(PORT, async () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+// app.listen(PORT, async () => {
+//     console.log(`Server is running on port ${PORT}`);
+// })
